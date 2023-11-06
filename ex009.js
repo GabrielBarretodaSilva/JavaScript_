13691551909080bfe8bcb4ac55a8411def1a77ab
Gabriel da Silva <gabrielleitebarretosilva2014@gmail.com>
2023-11-06 00:49:54 -0300

    ok

--- ex009.js
+++ ex009.js
@@ -1,6 +1,6 @@
 var pais = "Brasil"
 console.log (`vivendo em ${pais}`)
-if(pais != "Brasil") {
+if(pais == "Brasil") {
     console.log ('você é Brasileiro')
 }else{
     console.log ('você é Estrangeiro')
