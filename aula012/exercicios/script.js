4363967a19fc2e9dc163406607ed33be25806391
Gabriel da Silva <gabrielleitebarretosilva2014@gmail.com>
2023-11-25 03:24:43 -0300

    exercicio em andamento

--- aula012/exercicios/script.js
+++ aula012/exercicios/script.js
@@ -2,15 +2,17 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var imagem = window.document.getElementById('foto')
     var data = new Date()
-    //var hora = data.getHours()//
-    hora = 11
+    var hora = data.getHours()
     msg.innerText = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         imagem.src = 'dia.jpg'
+        document.body.style.backgroundColor = '#000099'
     } else if (hora >= 12 && hora < 18) {
         imagem.src = 'fim-de-tarde.jpg'
+        document.body.style.backgroundColor = '#009900'
     } else {
         imagem.src = 'dia.jpg'
+        document.body.style.backgroundColor = '#000000'
     }
 }
 
