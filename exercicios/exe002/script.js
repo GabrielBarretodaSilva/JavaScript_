299192323e2eb2232fa8fc997ab84d959fbbf555
Gabriel da Silva <gabrielleitebarretosilva2014@gmail.com>
2023-11-26 19:29:30 -0300

    finalizado

--- exercicios/exe002/script.js
+++ exercicios/exe002/script.js
@@ -10,13 +10,36 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(fsex[0].checked){
             genero = 'homem'
+            if(idade >= 0 && idade < 18){
+                //jovem
+                img.setAttribute('src', 'imagens/foto-homem-adolecente.png')
+            } else if(idade >= 18 && idade < 60){
+                //adulto
+                img.setAttribute('src', 'imagens/foto-homem-adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/foto-homem-idoso.png')
+            }
         } else if(fsex[1].checked){
             genero = 'mulher'
+            if(idade >= 0 && idade < 18){
+                //jovem
+                img.setAttribute('src', 'imagens/foto-mulher-adolecente.png')
+            } else if(idade >= 18 && idade < 60){
+                //adulto
+                img.setAttribute('src', 'imagens/foto-mulher-adulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/foto-mulher-idosa.png')
+            }
         }
-        res.style.textAlign = 'center'
+         res.style.textAlign = 'center'
          res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+         res.appendChild(img)
         
     }
 }
